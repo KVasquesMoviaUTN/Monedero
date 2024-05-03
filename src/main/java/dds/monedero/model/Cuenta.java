8d147e0f6ecfc2398764da9ecc9b88dda4b9825e
KVasquesMoviaUTN <kvasquesmovia@frba.utn.edu.ar>
2024-05-03 01:26:56 -0300

    fix 1

--- src/main/java/dds/monedero/model/Cuenta.java
+++ src/main/java/dds/monedero/model/Cuenta.java
@@ -15,8 +15,8 @@ public class Cuenta {
   private List<Movimiento> movimientos = new ArrayList<>();
 
   public Cuenta() {
-    saldo = 0;
-  }
+
+  }//Duplicated Code
 
   public Cuenta(double montoInicial) {
     saldo = montoInicial;
@@ -31,7 +31,7 @@ public class Cuenta {
       throw new MontoNegativoException(cuanto + ": el monto a ingresar debe ser un valor positivo");
     }
 
-    if (getMovimientos().stream().filter(movimiento -> movimiento.isDeposito()).count() >= 3) {
+    if (getMovimientos().stream().filter(movimiento -> movimiento.isDeposito()).count() >= 3) {//Message chains
       throw new MaximaCantidadDepositosException("Ya excedio los " + 3 + " depositos diarios");
     }
 
